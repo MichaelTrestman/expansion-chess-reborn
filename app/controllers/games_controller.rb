7bd0c7e226ee292d27ae2d56e54b5f4cab7e4ca1
MichaelTrestman <mtrestman@pivotal.io>
2019-05-23 09:59:46 +0200

    legit commit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,29 @@
 class GamesController < ApplicationController
 
   def index
-    config.firebase_client_data = {
-    url: "https://xchess-a3561.firebaseio.com",
-    private_key_json: File.open("/Users/michaeltrestman/keys/xchess-a3561-firebase-adminsdk-2hn8l-2e1b6600b5.json").read
-  }
-    firebase = Firebase::Client.new(config.firebase_client_data[:url], config.firebase_client_data[:private_key_json])
-    puts "firebase.get('games')"
-    @games = firebase.get('games').body
+
+
+    @games = firebase_client.get('games').body
   end
   def calculate_moves (board, chosen_piece)
 
   end
   def show
+    game_data = firebase_client.get("games/#{params[:id]}").body
+    board_stack = game_data["boardStack"]
+    @current_board_state = JSON.parse(board_stack.pop)
+    @board_height = @current_board_state["height"]
+    @board_width = @current_board_state["width"]
+  end
+
+  private
+
+  def firebase_client
+
+    config.firebase_client_data = {
+    url: "https://xchess-a3561.firebaseio.com",
+    private_key_json: File.open("/Users/michaeltrestman/keys/xchess-a3561-firebase-adminsdk-2hn8l-2e1b6600b5.json").read
+  }
+    @firebase = Firebase::Client.new(config.firebase_client_data[:url], config.firebase_client_data[:private_key_json])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'games/index'
-  get 'games/show'
+  resources :games, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
