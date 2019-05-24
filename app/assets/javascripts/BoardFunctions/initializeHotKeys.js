dd6f244cb4f6bde259c0756d33dacafa8eaa1238
MichaelTrestman <mtrestman@pivotal.io>
2019-05-24 14:26:53 +0200

    too much stuff!

---
+++ app/assets/javascripts/BoardFunctions/initializeHotKeys.js
@@ -0,0 +1,61 @@
+
+BoardFunctions.initializeHotKeys = function(){
+    $(document).on('keypress', function(e){
+    console.log('key pressed!')
+    var key = e.keyCode
+    console.log(key)
+    var $board = $('#board')
+    if (key == 93) {
+
+      var currentScale = $board.css('transform')
+
+      var matrixRegex = /matrix\((-?\d*\.?\d+),\s*0,\s*0,\s*(-?\d*\.?\d+),\s*0,\s*0\)/;
+
+      var matches = currentScale.match(matrixRegex);
+
+      var xScale = parseFloat(matches[1]);
+      var yScale = parseFloat(matches[2]);
+
+      xScale = ( xScale + 0.05 ).toString();
+      yScale = ( yScale + 0.05 ).toString();
+
+      $board.css('transform', 'scale(' + xScale + ',' + yScale + ')')
+    } else if (key == 91) {
+      var $board = $('#board')
+      var currentScale = $board.css('transform')
+
+      var matrixRegex = /matrix\((-?\d*\.?\d+),\s-?\d*\.?\d+,\s-?\d*\.?\d+,\s*(-?\d*\.?\d+),\s*\d*\.?\d+,\s*\d*\.?\d+\)/;
+
+      var matches = currentScale.match(matrixRegex);
+
+      var xScale = parseFloat(matches[1]);
+      var yScale = parseFloat(matches[2]);
+
+      xScale = ( xScale - 0.05 ).toString();
+      yScale = ( yScale - 0.05 ).toString();
+
+      $board.css('transform', 'scale(' + xScale + ',' + yScale + ')')
+    } else if ( key == 100) {
+
+      var left = $board.css('left').match(/\d+/)[0]
+      left = (parseInt(left) + 10).toString() + 'px';
+      $board.css('left', left)
+    } else if ( key == 97) {
+
+      var left = $board.css('left').match(/\d+/)[0]
+      left = (parseInt(left) - 10).toString() + 'px';
+      $board.css('left', left)
+    } else if ( key == 119) {
+      console.log('raising!')
+      var top = $board.css('top').match(/\d+/)[0]
+      top = (parseInt(top) - 10).toString() + 'px';
+      $board.css('top', top)
+      console.log($board.css('top'))
+    } else if ( key == 115) {
+      console.log('lowering!')
+      var top = $board.css('top').match(/\d+/)[0]
+      top = (parseInt(top) + 10).toString() + 'px';
+      $board.css('top', top)
+    }
+  })
+}
--- app/assets/javascripts/watchAndUpdateBoard.js
+++ app/assets/javascripts/watchAndUpdateBoard.js
@@ -5,8 +5,12 @@ BoardFunctions.watchAndUpdateBoard = function(boardPath){
 
 BoardFunctions.buildBoardFromSnapshot = function(snapshot){
   var gameData = snapshot.val();
+  BoardFunctions.startingBoardName = gameData['starting board name'];
   var playerSides = gameData.playerSides;
   var currentPiecePositions = gameData.boardStack[gameData.boardStack.length - 1];
+
+  BoardFunctions.currentPiecePositions = currentPiecePositions;
+
   if (currentPiecePositions==undefined) {
     throw "missing board :("
   }
@@ -14,6 +18,7 @@ BoardFunctions.buildBoardFromSnapshot = function(snapshot){
   BoardFunctions.setWalls(gameData.walls);
   BoardFunctions.setUpgradeSquares(gameData.upgradeSquares);
   BoardFunctions.setTurn(gameData.turn);
+  BoardFunctions.initializeSquares();
 }
 
 BoardFunctions.setWalls = function(walls){
@@ -21,6 +26,7 @@ BoardFunctions.setWalls = function(walls){
     var $square = $('div.square#posx-'+wall.posx+'posy-'+wall.posy).addClass('wall');
   })
 }
+
 BoardFunctions.setUpgradeSquares = function(upgradeSquares){
   $.each(upgradeSquares, function(_, upgradeSquare){
     var $square = $('div.square#posx-'+upgradeSquare.posx+'posy-'+upgradeSquare.posy).addClass('upgrade');
@@ -59,13 +65,55 @@ BoardFunctions.placePiece = function(_, piece){
   var $filledThings = $svg.find('[fill="ffffff"]');
   $filledThings.attr('fill', piece.side).css('fill', piece.side);
 
-
   $piece.append($svg)
+
   var $square = $('#posx-' + piece.posx +'posy-' + piece.posy);
   $square.append($piece);
-
 }
+
 BoardFunctions.setTurn = function(playerWhosTurn) {
-  console.log(playerWhosTurn)
+  BoardFunctions.turn = playerWhosTurn;
+  $('#turn-indicator').text(playerWhosTurn);
 }
 
+BoardFunctions.initializeSquares = function(){
+  var activeSquareSelector = '.square:has(div.piece.side-'+ BoardFunctions.turn  +')';
+
+  $(activeSquareSelector).each(BoardFunctions.initializeSquare);
+}
+
+BoardFunctions.initializeSquare = function(){
+  var $this = $(this);
+  $this.on('click', function(){
+    var $thisPiece = $this.find('.piece');
+
+
+
+    var selectedPieceData = {
+      posx: $thisPiece.attr('data-posX'),
+      posy: $thisPiece.attr('data-posY'),
+      side: $thisPiece.attr('data-side'),
+      type: $thisPiece.attr('data-type')
+    }
+    console.log(selectedPieceData)
+
+    var moves = BoardFunctions.getMoves(
+      BoardFunctions.startingBoardName,
+      BoardFunctions.currentPiecePositions,
+      selectedPieceData
+    );
+    $.each(moves, BoardFunctions.displayMove);
+  })
+}
+
+BoardFunctions.getMoves = function(startingBoardName, currentPiecePositions, selectedPiece){
+
+  var movesEndpoint = BoardFunctions.base_url;
+
+  //make request to calculate moves endpoint
+  return []
+}
+
+BoardFunctions.displayMove = function(){
+  console.log('attempting to display move :/');
+}
