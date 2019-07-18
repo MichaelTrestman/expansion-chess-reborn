ce107f2e07f3a74bd1db3389af827200d66093b6
MichaelTrestman <mtrestman@pivotal.io>
2019-07-18 10:06:37 +0200

    config to heroku mongo

--- app/controllers/concerns/game_data.rb
+++ app/controllers/concerns/game_data.rb
@@ -2,7 +2,7 @@ module GameData
   extend ActiveSupport::Concern
 
   def games_db
-    @games_db ||= Mongo::Client.new([ 'xchess-mongo-db:27017' ], :database => 'dev').database[:games]
+    @games_db ||= Mongo::Client.new([ 'mongodb://heroku_v95f0q1m:tjkh0h6ln8qn26vqiavli9qikr@ds253017.mlab.com:53017/heroku_v95f0q1m' ], :database => ENV.fetch('RAILS_ENV')).database[:games]
   end
 
   def starting_board
